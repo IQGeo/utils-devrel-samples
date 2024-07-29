9052618f10c365876dfd78f74f74750b723e2929
Jay Santos <jay.santos@iqgeo.com>
2024-07-29 16:05:49 +0100

    connect, disconnect, moveInto, isConduit working!

--- custom/public/js/ConduitChecker/conduitCheckerModal.js
+++ custom/public/js/ConduitChecker/conduitCheckerModal.js
@@ -2,6 +2,7 @@ import myw from 'myWorld-client';
 import React, { useState, useEffect } from 'react';
 import { DraggableModal, Button } from 'myWorld-client/react';
 import { Select, Space } from 'antd';
+import { merge } from 'jquery';
 
 export const ConduitCheckerModal = ({ open, plugin }) => {
     const [appRef] = useState(myw.app);
@@ -9,6 +10,9 @@ export const ConduitCheckerModal = ({ open, plugin }) => {
     const [conduits, setConduits] = useState([]);
     const [housings, setHousings] = useState([]);
     const [ugRoutes, setUgRoutes] = useState([]);
+    const [manholes, setManholes] = useState([]);
+    const [cabinets, setCabinets] = useState([]);
+    const [blownFiberTubes, setBlownFiberTubes] = useState([]);
     const [pickedFunction, setPickedFunction] = useState('');
     const [isOpen, setIsOpen] = useState(open);
 
@@ -24,6 +28,14 @@ export const ConduitCheckerModal = ({ open, plugin }) => {
         {
             value: 'connectConduits',
             label: 'connectConduits'
+        },
+        {
+            value: 'moveInto',
+            label: 'moveInto'
+        },
+        {
+            value: 'isContinuousConduitType',
+            label: 'isContinuousConduitType'
         }
     ];
 
@@ -40,7 +52,6 @@ export const ConduitCheckerModal = ({ open, plugin }) => {
 
         const housingPromises = [
             db.getFeatures('myworld/ug_route'),
-            db.getFeatures('myworld/oh_route'),
             db.getFeatures('myworld/conduit'),
             db.getFeatures('myworld/building'),
             db.getFeatures('myworld/mdu')
@@ -49,9 +60,17 @@ export const ConduitCheckerModal = ({ open, plugin }) => {
         Promise.all(housingPromises).then(results => {
             setHousings(results.flat());
         });
-        // db.getFeatures('myworld/ug_route').then(result => {
-        //     setUgRoutes(result);
-        // });
+        db.getFeatures('myworld/manhole').then(result => {
+            setManholes(result);
+        });
+
+        db.getFeatures('myworld/cabinet').then(result => {
+            setCabinets(result);
+        });
+
+        db.getFeatures('myworld/blown_fiber_tube').then(result => {
+            setBlownFiberTubes(result);
+        });
     }, []);
 
     const closeWindow = () => {
@@ -59,50 +78,95 @@ export const ConduitCheckerModal = ({ open, plugin }) => {
     };
 
     const okButton = () => {
-        for (const c in conduits) {
-            console.log(conduits[c]);
-            console.log(conduits[c]._myw.title);
-        }
-        for (const r in ugRoutes) {
-            console.log(ugRoutes[r]);
+        for (const r in manholes) {
+            console.log(manholes[r]);
         }
     };
 
     const onDisconnectConduit = () => {
-        const index = Math.floor(Math.random() * conduits.length);
-        const housingId = parseInt(conduits[index].properties.housing.split('/')[1]);
-        const housing = housings.find(item => item.id === housingId);
-        console.log(conduits[index]._myw.title + ' is housed into ' + housing._myw.title);
+        const conduit = blownFiberTubes.find(conduit =>
+            conduit.properties.name.includes('JS_BF_2')
+        );
+        const housing = cabinets.find(cabinet => cabinet.properties.name.includes('JS_CAB_2'));
+        console.log(conduit.properties.name + ' is housed into ' + housing.properties.name);
         plugin
-            .disconnectConduit(conduits[index], housing)
+            .disconnectConduit(conduit, housing)
             .then(result => {
                 console.log('disconnectConduit successful!');
                 console.log(result);
             })
             .catch(alert);
+        appRef.setCurrentFeature(housing, { zoomTo: true });
     };
 
     const onConnectConduits = () => {
-        const conduit1index = Math.floor(Math.random() * conduits.length);
-        const conduit2index = Math.floor(Math.random() * conduits.length);
-        const housingId = parseInt(conduits[conduit1index].properties.housing.split('/')[1]);
-        const housing = housings.find(item => item.id === housingId);
-        // console.log(conduits[index]._myw.title + ' is housed into ' + housing.properties.name);
+        const disconnectedBlownFiberTubes = blownFiberTubes.filter(conduit =>
+            conduit.properties.name.includes('JS_BF_2')
+        );
+
+        const housing = cabinets.find(cabinet => cabinet.properties.name.includes('JS_CAB_2'));
+
         console.log(
             'Connecting ' +
-                conduits[conduit1index]._myw.title +
+                disconnectedBlownFiberTubes[0].properties.name +
                 ' and ' +
-                conduits[conduit2index]._myw.title +
+                disconnectedBlownFiberTubes[1].properties.name +
                 ' into ' +
-                housing._myw.title
+                housing.properties.name
         );
         plugin
-            .connectConduits(housing, conduits[conduit1index], conduits[conduit2index])
+            .connectConduits(
+                housing,
+                disconnectedBlownFiberTubes[0],
+                disconnectedBlownFiberTubes[1]
+            )
             .then(result => {
                 console.log('onConnectConduits successful!');
                 console.log(result);
             })
-            .catch(alert);
+            .catch(result => {
+                console.log(result);
+            });
+
+        appRef.setCurrentFeature(housing, { zoomTo: true });
+    };
+
+    const onMoveInto = () => {
+        const subConduit = conduits.find(conduit =>
+            conduit.properties.name.includes('JS_SUBCND_1')
+        );
+        const housingId = Number(subConduit.properties.housing.split('/')[1]);
+        let destinationHousing;
+        console.log(subConduit);
+        console.log(housingId);
+        const housing = housings.find(housing => housing.properties.id === housingId);
+        console.log(housing);
+        if (housing.properties.name === 'JS_CND_1')
+            destinationHousing = housings.find(housing => housing.properties.name === 'JS_CND_5');
+        else destinationHousing = housings.find(housing => housing.properties.name === 'JS_CND_1');
+        console.log(
+            subConduit.properties.name +
+                ' is currently housed into ' +
+                housing.properties.name +
+                ' and will be moved to ' +
+                destinationHousing.properties.name
+        );
+        plugin.moveInto(subConduit, destinationHousing).then(result => {
+            console.log('moveInto successful!');
+            console.log(result);
+        });
+        appRef.setCurrentFeature(destinationHousing, { zoomTo: true });
+    };
+
+    const onIsContinuousConduitType = () => {
+        const mergedConduits = conduits.concat(blownFiberTubes);
+        const randomConduit = mergedConduits[Math.floor(Math.random() * mergedConduits.length)];
+
+        if (plugin.isContinuousConduitType(randomConduit)) {
+            console.log('Conduit ' + randomConduit.properties.name + ' is continuous');
+        } else {
+            console.log('Conduit ' + randomConduit.properties.name + ' is NOT continuous');
+        }
     };
 
     const onListConduits = () => {
@@ -130,14 +194,18 @@ export const ConduitCheckerModal = ({ open, plugin }) => {
                     <div>
                         <Space direction="vertical" size="small">
                             <p>
+                                IMPORTANT: disconnectConduit only works on Continuous conduits
+                                (e.g.: Blown Fiber Tubes)
+                            </p>
+                            <p>
                                 disconnectConduit disconnects the conduit from its housing. The
                                 conduit's housing is stored in the root_housing property of the
                                 conduit.
                             </p>
                             <p>
-                                Pressing the button will randomly pick a conduit and disconnect it
-                                from its housing, the operation information is shown in the
-                                development console. Returns true if the operation was successful.
+                                Pressing the button will disconnect conduit JB_BFT_2 in the housing
+                                JS_CAB_2, focus the map on the cabinet and show the cabinet status
+                                on the details.
                             </p>
                             <Button type="primary" onClick={onDisconnectConduit}>
                                 disconnectConduit
@@ -150,16 +218,62 @@ export const ConduitCheckerModal = ({ open, plugin }) => {
                     <div>
                         <Space direction="vertical" size="small">
                             <p>
-                                connectConduits a housing and two conduits, and connects the
-                                conduits into the housing.
+                                IMPORTANT: connectConduits only works on Continuous conduits (e.g.:
+                                Blown Fiber Tubes)
                             </p>
                             <p>
-                                Pressing the button will randomly pick two conduits and connect them
-                                within the housing of one of them. Returns true if the operation was
-                                successful.
+                                connectConduit connects two conduits arriving in a housing. The
+                                conduit's housing is stored in the root_housing property of the
+                                conduit.
+                            </p>
+                            <p>
+                                Pressing the button will connect the two sections of conduit
+                                JB_BFT_2 in the housing JS_CAB_2, focus the map on the cabinet and
+                                show the cabinet status on the details. Trying to connect the
+                                conduits when they are already connected will make the promise throw
+                                an error that will be printed in the console.
                             </p>
                             <Button type="primary" onClick={onConnectConduits}>
                                 connectConduits
+                            </Button>
+                        </Space>
+                    </div>
+                );
+            case 'moveInto':
+                return (
+                    <div>
+                        <Space direction="vertical" size="small">
+                            <p>
+                                moveInto moves a cable segment or conduit to a new housing in same
+                                route
+                            </p>
+                            <p>
+                                Pressing the button will move JS_SUBCND_1 between JS_CND_1 to the
+                                conduit JS_CND_5 (i.e.: If it is currently housed into JS_CND_1 it
+                                will move it to JS_CND_5 and vice-versa), focus the map on the
+                                cabinet and show the route the conduit information in the details
+                                tab.
+                            </p>
+                            <Button type="primary" onClick={onMoveInto}>
+                                moveInto
+                            </Button>
+                        </Space>
+                    </div>
+                );
+            case 'isContinuousConduitType':
+                return (
+                    <div>
+                        <Space direction="vertical" size="small">
+                            <p>
+                                isContinuousConduitType will return true if the feature is a
+                                continuous conduit (e.g.: Blown Fiber Tubes).
+                            </p>
+                            <p>
+                                Pressing the button will randomly pick a conduit and check if it is
+                                continuous or not.
+                            </p>
+                            <Button type="primary" onClick={onIsContinuousConduitType}>
+                                isContinuousConduitType
                             </Button>
                         </Space>
                     </div>
