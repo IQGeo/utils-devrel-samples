62feaaffea7828b00a0b47e31f5f94a5d0cad25b
Jay Santos <jay.santos@iqgeo.com>
2025-08-05 15:43:22 +0100

    Re-added WFM/NMT - NOT WORKING

--- public/js/main.nmt_wfm_integration.js
+++ public/js/main.nmt_wfm_integration.js
@@ -1,6 +1,6 @@
 import myw from 'myWorld-client';
 
-import '../../../workflow_manager/public/js/main.wfm';
+import 'modules/workflow_manager/js/main.wfm';
 
 import { FieldValidatorPlugin } from '../Samples/WFM_NMT_integration/fieldValidatorPlugin';
 
@@ -10,7 +10,6 @@ const desktopLayoutDef = myw.applicationDefinition.layouts.desktop;
 const plugins = myw.applicationDefinition.plugins; //this is the application's list of plugins
 
 plugins['fieldValidatorPlugin'] = FieldValidatorPlugin; //Adding the newly created plugin to the application's array
-
 
 const desktopToolbarButtons = desktopLayoutDef.controls.toolbar[1].buttons; //This is the list of buttons in the application's top toolbar
 desktopToolbarButtons.push('fieldValidatorPlugin.dialog');
