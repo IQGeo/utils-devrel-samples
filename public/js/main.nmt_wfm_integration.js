2b9654682a1ad22d9243dbfe4ff7afdbdc59c0af
timoneyb-iqgeo <brian.timoney@iqgeo.com>
2025-10-30 21:40:01 -0600

    updated main application page for WFM/WMT integration example to include map widget tool with custom tool on toolbar

--- public/js/main.nmt_wfm_integration.js
+++ public/js/main.nmt_wfm_integration.js
@@ -3,6 +3,7 @@ import myw from 'myWorld-client';
 import 'modules/workflow_manager/js/main.wfm';
 
 import { FieldValidatorPlugin } from '../Samples/WFM_NMT_integration/fieldValidatorPlugin';
+import { MilestoneMapPlugin } from '../Samples/WFM_NMT_map_widget/milestoneMapPlugin';
 
 myw.localisation.loadModuleLocale('utils-devrel-samples');
 
@@ -10,6 +11,8 @@ const desktopLayoutDef = myw.applicationDefinition.layouts.desktop;
 const plugins = myw.applicationDefinition.plugins; //this is the application's list of plugins
 
 plugins['fieldValidatorPlugin'] = FieldValidatorPlugin; //Adding the newly created plugin to the application's array
+plugins['milestoneMapPlugin'] = MilestoneMapPlugin;
 
 const desktopToolbarButtons = desktopLayoutDef.controls.toolbar[1].buttons; //This is the list of buttons in the application's top toolbar
 desktopToolbarButtons.push('fieldValidatorPlugin.dialog');
+desktopToolbarButtons.push('milestoneMapPlugin.dialog');
