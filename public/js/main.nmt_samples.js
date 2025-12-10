ccd0b7ea21f8465288d2698a8071b07ee4ca52eb
Birwa balar <balarbirwa@gmail.com>
2025-12-10 09:07:01 -0700

    added rest api plugin to its own branch

--- public/js/main.nmt_samples.js
+++ public/js/main.nmt_samples.js
@@ -8,6 +8,7 @@ import { LrtPlugin } from '../Samples/LRT/lrt_plugin';
 import { BFTubeRenamePlugin } from '../Samples/bf_tube_rename/bf_tube_rename_plugin';
 import { LiveDocsPlugin } from '../Samples/live_docs/liveDocs_plugin';
 import { ConduitCapacityPlugin } from '../Samples/conduit_capacity/conduit_capacity_plugin';
+import { restApiPlugin } from '../Samples/rest_api_plugin/rest_api_plugin';
 
 import fiberCountRule from '../Samples/design_rules/fiberCountRule';
 
@@ -28,6 +29,7 @@ plugins['lrtPlugin'] = LrtPlugin;
 plugins['bfTubeRenamePlugin'] = BFTubeRenamePlugin;
 plugins['liveDocsPlugin'] = LiveDocsPlugin;
 plugins['conduitCapacityPlugin'] = ConduitCapacityPlugin;
+plugins['restApiPlugin'] = restApiPlugin;
 
 plugins['designRulesManager'][1].rules.push(fiberCountRule);
 
@@ -40,7 +42,8 @@ plugins['palette'] = [
             'lrtPlugin.dialog',
             'bfTubeRenamePlugin.dialog',
             'liveDocsPlugin.dialog',
-            'conduitCapacityPlugin.dialog'
+            'conduitCapacityPlugin.dialog',
+            'restApiPlugin.dialog'
         ]
     }
 ];
